72ed749c1599a5874b59110e03982f27a76af063
MariCeo19 <marianathayza3@gmail.com>
2023-05-02 17:02:56 -0300

    atualizaçao 02/05/2023

--- app/src/main/java/com/example/applimonada/MainActivity.kt
+++ app/src/main/java/com/example/applimonada/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.applimonada
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.applimonada.ui.theme.AppLimonadaTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,27 +34,41 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                        color = MaterialTheme.colorScheme.background) {
+
+                  Redondolimao ()
+
                 }
             }
         }
     }
 }
-
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Redondolimao() {
+   Column(
+       verticalArrangement = Arrangement.Center,
+       horizontalAlignment = Alignment.CenterHorizontally,
+       modifier = Modifier.fillMaxSize()
+   ) {
+       Text(
+           text = stringResource(R.string.limoeiro),
+           fontSize = 20.sp,
+           modifier = Modifier.padding(bottom = 15.dp)
+       )
+
+       Image(
+           painter = painterResource(id = R.drawable.limoeiro),
+           contentDescription = null,
+           modifier = Modifier
+               .size(300.dp)
+               .border(
+                   BorderStroke(2.dp, Color.Cyan),
+                   RoundedCornerShape(15.dp)
+               )
+       )
+   }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppLimonadaTheme {
-        Greeting("Android")
-    }
-}
+
+
