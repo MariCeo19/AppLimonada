94fdd526017ae1c0b69af9c2422fc3985894da8a
MariCeo19 <marianathayza3@gmail.com>
2023-05-04 17:28:54 -0300

    atualizaçao 04/05/2023

--- app/src/main/java/com/example/applimonada/MainActivity.kt
+++ app/src/main/java/com/example/applimonada/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +17,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -46,29 +51,66 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun Redondolimao() {
-   Column(
-       verticalArrangement = Arrangement.Center,
-       horizontalAlignment = Alignment.CenterHorizontally,
-       modifier = Modifier.fillMaxSize()
-   ) {
-       Text(
-           text = stringResource(R.string.limoeiro),
-           fontSize = 20.sp,
-           modifier = Modifier.padding(bottom = 15.dp)
-       )
 
-       Image(
-           painter = painterResource(id = R.drawable.limoeiro),
-           contentDescription = null,
-           modifier = Modifier
-               .size(300.dp)
-               .border(
-                   BorderStroke(2.dp, Color.Cyan),
-                   RoundedCornerShape(15.dp)
-               )
-       )
-   }
+   var tela by remember { mutableStateOf(value = 1) }
+
+    when(tela) {
+        1 ->  Conteudolimao(
+            R.string.limoeiro,
+            R.drawable.limoeiro,
+            onImageClick = {
+                tela = 2
+            }
+        )
+        2 ->  Conteudolimao(
+            R.string.limao,
+            R.drawable.limonada,
+            onImageClick = {
+                tela = 3
+            }
+        )
+        3 ->  Conteudolimao(
+            R.string.copo_de_limonada,
+            R.drawable.copo_limonada,
+                onImageClick = {
+                    tela = 4
+                }
+        )
+        4 ->  Conteudolimao(
+            R.string.copo_vazio,
+            R.drawable.reiniciar,
+                    onImageClick = {
+                        tela = 1
+                    }
+        )
+    }
 }
+@Composable
+fun Conteudolimao(recursoTextoId: Int, recursoImageId: Int, onImageClick:()->Unit){
 
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(recursoTextoId),
+            fontSize = 20.sp,
+            modifier = Modifier.padding(bottom = 15.dp)
+        )
+
+        Image(
+            painter = painterResource(id = recursoImageId),
+            contentDescription = null,
+            modifier = Modifier
+                .size(300.dp)
+                .border(
+                    BorderStroke(2.dp, Color.Cyan),
+                    RoundedCornerShape(15.dp)
+                )
+                .clickable(onClick =onImageClick)
+        )
+    }
+}
 
 
